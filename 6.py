a3bd0ac939aafc5ebba230a751de572539e6d317
zyando <zyando.iron79@gmail.com>
2020-02-01 00:24:51 +0900

    Fix

--- 6.py
+++ 6.py
@@ -4,8 +4,8 @@ result = [0 for i in range(801)]
 
 for i in range(N):
     f, l = list(map(int, input().split()))
-    f -= 800
-    l -= 800
+    f -= 900
+    l -= 900
 
     result = [result[i] + (1 if f <= i <= l else 0) for i in range(801)]
 
